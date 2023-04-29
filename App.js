481c5da07e01cb069fd973de34204ad821c2caea
svetoslav <svetoslav.shterev@monday-consulting.com>
2023-04-29 16:12:14 +0200

    beta: changes by App.js

--- App.js
+++ App.js
@@ -1,22 +1,67 @@
 import React from "react"
-import { View, StyleSheet, Text  } from 'react-native'
-import CurrentWeather from "./src/screens/CurrentWeather"
-import City from "./src/screens/City"
-import UpcomingWeather from "./src/screens/UpcomingWeather"
+import CurrentWeather from './src/screens/CurrentWeather'
+import City from './src/screens/City'
+import UpcomingWeather from './src/screens/UpcomingWeather'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
 
+const Tab = createBottomTabNavigator()
 
 const App = () => {
+  
   return (
-    <View style={styles.container}>
-        <UpcomingWeather/>
-    </View>
+  <NavigationContainer>
+    <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: 'purple',
+      tabBarInactiveTintColor: 'grey'
+    }}
+    >
+      <Tab.Screen 
+      name={'City'} 
+      component={City}
+      options={{
+        tabBarIcon: ({focused}) => (
+        <Feather
+        name={'home'}
+        size={25}
+        color={focused ? 'purple' : 'black'}
+        />
+        )
+      }}/>
+      <Tab.Screen 
+      name={'Upcoming'} 
+      component={UpcomingWeather}
+      options={{
+        tabBarIcon: ({focused}) => (
+          <Feather
+          name={'clock'}
+          size={25}
+          color={focused ? 'purple' : 'black'}
+          />
+        )
+      }}
+      />
+      <Tab.Screen 
+      name={'Current'} 
+      component={CurrentWeather}
+      options={{
+        tabBarIcon: ({focused}) => (
+          <Feather
+          name={'droplet'}
+          size={25}
+          color={focused ? 'purple' : 'black'}
+          />
+        )
+      }}
+      />
+   
+    </Tab.Navigator>
+  </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1
-  }
-})
+
 
 export default App
